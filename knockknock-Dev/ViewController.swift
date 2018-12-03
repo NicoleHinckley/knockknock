b954386f9cffaad3640f1c3908561b93a31b905d
E Nicole Hinckley <nicolehinckley94@gmail.com>
2018-12-03 07:17:14 -0500

    Initial Commit

---
+++ knockknock-Dev/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  knockknock-Dev
+//
+//  Created by Nicole Hinckley on 12/3/18.
+//  Copyright © 2018 Nicole Hinckley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
